6843fe6e8400d23bfd3a22a8e61659023c53dab0
Pradeepkumar <pradeep@shoptrade.co>
2022-10-16 20:12:55 +0530

    About us page loop marquee

--- assets/custom.js
+++ assets/custom.js
@@ -52,177 +52,175 @@ function hideVideo(index, e) {
 }
 
 $(document).ready(function () {
-  AOS.init({
-    duration: 1200,
-  })
-
-  let items = document.querySelectorAll('.menu-mega-nav li');
-  items.forEach( item => item.addEventListener('mouseenter', function() {
-    handleHover(this, items)
-  }))
-
-  function handleHover(el, objects) {
-    items.forEach(item => {
-      item.classList.remove('active')      
+    AOS.init({
+        duration: 1200,
     })
-    el.classList.add('active')
-  }
-  $('.custom_sub_button').click(function(){
-    $(this).closest('.block-inner').find('.block-inner-card-info').addClass('add_info_sub');
-  });
-  $('.close_card_info').click(function(){
-    $(this).closest('.block-inner').find('.block-inner-card-info').removeClass('add_info_sub');
-  });
-  
-  $('.readmore').click(function (event) {
-    event.preventDefault();
-    var descriptionFull = document.querySelector('.product-description-full');
-    descriptionFull.style.display = 'block';
-    var descriptionShort = document.querySelector('.product-description-short');
-    descriptionShort.style.display = 'none';
-  });
-  $('.readless').click(function (event) {
-    event.preventDefault();
-    var descriptionFull = document.querySelector('.product-description-full');
-    descriptionFull.style.display = 'none';
-    var descriptionShort = document.querySelector('.product-description-short');
-    descriptionShort.style.display = 'block';
-  });  
-  
-  //sub collection image
-  $('.subcollection_slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 8,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
-    nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
-    responsive: [
-        {
-            breakpoint: 1680,
+        
+    let items = document.querySelectorAll('.menu-mega-nav li');
+    items.forEach( item => item.addEventListener('mouseenter', function() {
+        handleHover(this, items)
+    }))
+
+    function handleHover(el, objects) {
+        items.forEach(item => {
+        item.classList.remove('active')      
+        })
+        el.classList.add('active')
+    }
+    $('.custom_sub_button').click(function(){
+        $(this).closest('.block-inner').find('.block-inner-card-info').addClass('add_info_sub');
+    });
+    $('.close_card_info').click(function(){
+        $(this).closest('.block-inner').find('.block-inner-card-info').removeClass('add_info_sub');
+    });
+    
+    $('.readmore').click(function (event) {
+        event.preventDefault();
+        var descriptionFull = document.querySelector('.product-description-full');
+        descriptionFull.style.display = 'block';
+        var descriptionShort = document.querySelector('.product-description-short');
+        descriptionShort.style.display = 'none';
+    });
+    $('.readless').click(function (event) {
+        event.preventDefault();
+        var descriptionFull = document.querySelector('.product-description-full');
+        descriptionFull.style.display = 'none';
+        var descriptionShort = document.querySelector('.product-description-short');
+        descriptionShort.style.display = 'block';
+    });  
+    
+    //sub collection image
+    $('.subcollection_slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+        nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+        responsive: [
+            {
+                breakpoint: 1680,
+                settings: {
+                slidesToShow: 7,
+                slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1300,
+                settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+            breakpoint: 600,
             settings: {
-              slidesToShow: 7,
-              slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 1300,
-            settings: {
-              slidesToShow: 6,
-              slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 769,
-            settings: {
-                slidesToShow: 4,
+                arrows: false,
+                slidesToShow: 3,
                 slidesToScroll: 1
             }
+            }
+        ]
+    });
+    $(".subcollection_slider").show();
+    $('.navigation__item .country_selector').click(() => {
+        $('.navigation__item .country_selector').toggleClass('active');
+        $('.navigation__item .country_selector #dropdown_linebreak').toggleClass('show');
+    })
+
+    $('.testimonial_list__inner').slick({
+        infinite: false,
+        slidesToShow: 1.6,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-product-prev" aria-label=""><svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M11.913,23.827l-1.564-1.6,9.2-9.2H0V10.8H19.545l-9.2-9.2L11.913,0,23.827,11.913Z" transform="translate(23.827 23.827) rotate(180)"/></svg></button>',
+        nextArrow: '<button type="button" class="slick-product-next" aria-label=""><svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M19.913,31.827l-1.564-1.6,9.2-9.2H8V18.8H27.545l-9.2-9.2L19.913,8,31.827,19.913Z" transform="translate(-8 -8)"/></svg></button>',
+        responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+            slidesToShow: 1.4,
+            slidesToScroll: 1,
+            arrows: false,
+            }
         },
         {
-        breakpoint: 600,
-        settings: {
+            breakpoint: 1112,
+            settings: {
+            slidesToShow: 2.2,
+            slidesToScroll: 1,
             arrows: false,
-            slidesToShow: 3,
-            slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+            slidesToShow: 2.2,
+            slidesToScroll: 1,
+            arrows: false,
+            }
+        },
+        {
+            breakpoint: 625,
+            settings: {
+            slidesToShow: 1.2,
+            slidesToScroll: 1,
+            arrows: false,
+            }        
         }
-        }
-      ]
-  });
-  $(".subcollection_slider").show();
-  $('.navigation__item .country_selector').click(() => {
-    $('.navigation__item .country_selector').toggleClass('active');
-      $('.navigation__item .country_selector #dropdown_linebreak').toggleClass('show');
-  })
+    
+        ]
+    });
+    updateContainer();
+    scrollAbout();
+    $(window).resize(function() {
+        updateContainer();
+        scrollAbout();
+    });
+    var figure2 = $(".chill-videos-item");
+    var vid = figure2.find(".play-video");
+    var videoNo = vid.length;
 
-  $('.testimonial_list__inner').slick({
-    infinite: false,
-    slidesToShow: 1.6,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow: '<button type="button" class="slick-product-prev" aria-label=""><svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M11.913,23.827l-1.564-1.6,9.2-9.2H0V10.8H19.545l-9.2-9.2L11.913,0,23.827,11.913Z" transform="translate(23.827 23.827) rotate(180)"/></svg></button>',
-    nextArrow: '<button type="button" class="slick-product-next" aria-label=""><svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M19.913,31.827l-1.564-1.6,9.2-9.2H8V18.8H27.545l-9.2-9.2L19.913,8,31.827,19.913Z" transform="translate(-8 -8)"/></svg></button>',
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 1.4,
-          slidesToScroll: 1,
-          arrows: false,
-        }
-      },
-      {
-        breakpoint: 1112,
-        settings: {
-          slidesToShow: 2.2,
-          slidesToScroll: 1,
-          arrows: false,
-        }
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 2.2,
-          slidesToScroll: 1,
-          arrows: false,
-        }
-      },
-      {
-        breakpoint: 625,
-        settings: {
-          slidesToShow: 1.2,
-          slidesToScroll: 1,
-          arrows: false,
-        }        
-      }
-  
-    ]
-  });
-  updateContainer();
-  scrollAbout();
-  $(window).resize(function() {
-      updateContainer();
-      scrollAbout();
-  });
-  var figure2 = $(".chill-videos-item");
-  var vid = figure2.find(".play-video");
-  var videoNo = vid.length;
-
-  for (let i = 1; i <= videoNo; i++){
-      let iframees = document.querySelector('#player-'+i);
-      let playeer1 = new Vimeo.Player(iframees);
-      playeer1.getDuration().then(function(duration) {
-          var ids =  document.querySelector('#player-'+i);
-          ids.parentElement.parentElement.querySelector('.video-length').innerHTML  = duration.toFixed(0)+' sec';
-      });
-  }  
+    for (let i = 1; i <= videoNo; i++){
+        let iframees = document.querySelector('#player-'+i);
+        let playeer1 = new Vimeo.Player(iframees);
+        playeer1.getDuration().then(function(duration) {
+            var ids =  document.querySelector('#player-'+i);
+            ids.parentElement.parentElement.querySelector('.video-length').innerHTML  = duration.toFixed(0)+' sec';
+        });
+    }  
   
 //   $('.add_to_cart_btn').click(function(){    
 //     addItemToCart( $(this).attr("data-product-id") , 1)
 //   });
 
-//   function addItemToCart(variant_id, qty) {
-//     data = {
-//       "id": variant_id,
-//       "quantity": qty
-//     }
-//     jQuery.ajax({
-//       type: 'POST',
-//       url: '/cart/add.js',
-//       data: data,
-//       dataType: 'json',
-//       success: function() { 
-//         document.documentElement.dispatchEvent(new CustomEvent('cart:refresh', {
-//         bubbles: true  
-//        }));
-//       }
-//     });
-//     document.documentElement.dispatchEvent(new CustomEvent('cart:refresh', {
-//     	bubbles: true 
-//  	 }));
-//   }
+// const animation_items = [...document.getElementsByClassName('list__item')];
+    const containerElem = document.getElementById('containerElem');
+    const leftSideOfContainer = containerElem.getBoundingClientRect().left;
+    const listElem = document.getElementById('list');
+    let currentLeftValue = 0;
+            
+    window.setInterval(animationLoop, 10);    
+    function animationLoop() {
+        const firstListItem = listElem.querySelector('.list__item:first-child');      
+        let rightSideOfFirstItem = firstListItem.getBoundingClientRect().right;
+        if(rightSideOfFirstItem == leftSideOfContainer){
+        currentLeftValue = -1;
+        listElem.appendChild(firstListItem);
+        }      
+        listElem.style.marginLeft = `${currentLeftValue}px`;
+        currentLeftValue--;
+    }
+
 
 });
 
