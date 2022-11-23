89abae4569f725639a9624bc324c05b00dc864f3
prathi sahrudh <sahrudhprathi@hotmail.co.uk>
2022-11-24 00:58:26 +0530

    Update Announcement Bar

--- assets/custom.js
+++ assets/custom.js
@@ -666,4 +666,20 @@ const featureProductSubscriptionUtil = (function () {
 
 document.addEventListener('DOMContentLoaded', () => {
     featureProductSubscriptionUtil.EventHandler();
+    AnnouncementBarSlider();
 })
+
+
+const AnnouncementBarSlider = function () {
+  $(".Newannouncement-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 600,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+  });
+};
