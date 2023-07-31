2981b2c754d24f26e53d50421538c67289e034d5
Keerthi-Sindhura <keerthi@shoptrade.co>
2023-07-31 18:45:55 +0530

    Content updated on new vape page

--- assets/custom.js
+++ assets/custom.js
@@ -72,7 +72,7 @@ $(document).ready(function () {
         slidesToScroll: 1,
         arrows: true,
         draggable:true,
-        infinite: false,
+        infinite: true,
         prevArrow: '<button type="button" class="slick-flavour-prev" aria-label=""><svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M11.913,23.827l-1.564-1.6,9.2-9.2H0V10.8H19.545l-9.2-9.2L11.913,0,23.827,11.913Z" transform="translate(23.827 23.827) rotate(180)"/></svg></button>',
         nextArrow: '<button type="button" class="slick-flavour-next" aria-label=""><svg xmlns="http://www.w3.org/2000/svg" width="23.827" height="23.827" viewBox="0 0 23.827 23.827"><path id="arrow_forward_FILL0_wght400_GRAD0_opsz48" d="M19.913,31.827l-1.564-1.6,9.2-9.2H8V18.8H27.545l-9.2-9.2L19.913,8,31.827,19.913Z" transform="translate(-8 -8)"/></svg></button>',
         responsive: [
